298290bdec3a6825b56d3fec5602bd871cf7b093
sangpk <dinhsangcnc@gmail.com>
2022-12-25 00:23:35 +0700

    file

---
+++ src/main/java/codeSang/service/ICustomerService.java
@@ -0,0 +1,16 @@
+package codeSang.service;
+
+
+import codeSang.models.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+
+        List<Customer> findAll();
+        void save(Customer customer);
+        Customer findById(int id);
+        void update(int id, Customer customer);
+        void remove(int id);
+
+}
